4edd6dc6a66fcb0390a8573f4adc8ffa52e28cf5
JakubTuta <jakubtuta@o2.pl>
2023-05-03 16:16:46 +0200

    huge code refactor

--- wizualizacja2pg.py
+++ wizualizacja2pg.py
@@ -1,206 +1,180 @@
-from tkinter import *
-import pygame
+from tkinter import *           # pip install tkinter
+import pygame                   # pip install pygame    /   pip install pygame --pre
 from random import shuffle
 from time import sleep
+import pyautogui                # pip install pyautogui
 
-ILOSC_LICZB = 0
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-GRAY = (100, 100, 100)
-
-
-def zamknij(odpowiedz):
-
-    global ILOSC_LICZB
-    try:
-        ILOSC_LICZB = int(odpowiedz)
-    except TypeError:
-        return
-    
-    if ILOSC_LICZB < 1 or ILOSC_LICZB > 512:
-        return
-
-    top.destroy()
+SETTINGS = {}
+COLORS = {
+    "WHITE": (255, 255, 255),
+    "GREEN": (0, 255, 0),
+    "GRAY": (100, 100, 100)
+}
+WIDTH, HEIGHT = pyautogui.size()
 
 
-top = Tk()
-top.title("Ilość liczb")
-top.resizable(False, False)
-top.attributes('-topmost', True)
+def settings():
+    def zamknij(algorythm, iloscLiczb, delay):
+        try:
+            iloscLiczb = int(iloscLiczb)
+        except:
+            return
+        
+        if iloscLiczb < 1 or iloscLiczb > 512:
+            return
+        
+        SETTINGS["algorythm"] = algorythm
+        SETTINGS["iloscLiczb"] = iloscLiczb
+        SETTINGS["delay"] = delay
 
-Label(top, text="Podaj ilość liczb:", font=("Times New Roman", 20)).grid(row=0, column=0, padx=20, pady=10)
-Label(top, text="(1 - 512)", font=("Times New Roman", 15, "italic")).grid(row=1, column=0, padx=10, pady=10)
-Label(top, text="Wybierz rodzaj sortowania:", font=("Times New Roman", 20)).grid(row=0, column=1, padx=20, pady=10)
+        top.destroy()
 
-var = StringVar(value="bubble")
+    top = Tk()
+    top.title("Ilość liczb")
+    top.resizable(False, False)
 
-Radiobutton(top, text="Sortowanie bąbelkowe", variable=var, value="bubble", font=("Times New Roman", 15)).grid(row=1, column=1, padx=10, pady=10, sticky=W)
-Radiobutton(top, text="Sortowanie przez wstawianie", variable=var, value="insertion", font=("Times New Roman", 15)).grid(row=2, column=1, padx=10, pady=10, sticky=W)
-Radiobutton(top, text="Sortowanie przez wybieranie", variable=var, value="selection", font=("Times New Roman", 15)).grid(row=3, column=1, padx=10, pady=10, sticky=W)
-Radiobutton(top, text="Bogo sort", variable=var, value="bogo", font=("Times New Roman", 15)).grid(row=4, column=1, padx=10, pady=10, sticky=W)
-Radiobutton(top, text="Cocktail shaker sort", variable=var, value="shaker", font=("Times New Roman", 15)).grid(row=5, column=1, padx=10, pady=10, sticky=W)
-Radiobutton(top, text="Quicksort", variable=var, value="quick", font=("Times New Roman", 15)).grid(row=6, column=1, padx=10, pady=10, sticky=W)
-Radiobutton(top, text="Sortowanie grzebieniowe (comb sort)", variable=var, value="comb", font=("Times New Roman", 15)).grid(row=7, column=1, padx=10, pady=10, sticky=W)
-Radiobutton(top, text="Sortowanie pozycyjne (radix sort)", variable=var, value="radix", font=("Times New Roman", 15)).grid(row=8, column=1, padx=10, pady=10, sticky=W)
+    Label(top, text="Podaj ilość liczb:", font=("Times New Roman", 20)).grid(row=0, column=0, padx=20, pady=10)
+    Label(top, text="(1 - 512)", font=("Times New Roman", 15, "italic")).grid(row=1, column=0, padx=10, pady=10)
+    Label(top, text="Wybierz rodzaj sortowania:", font=("Times New Roman", 20)).grid(row=0, column=1, padx=20, pady=10)
 
-odpowiedz = Entry(top, font=("Times New Roman", 15))
-odpowiedz.grid(row=2, column=0, padx=10, pady=10, ipadx=10)
+    sortingAlgorythm = StringVar(value="bubble")
+    Radiobutton(top, text="Sortowanie bąbelkowe", variable=sortingAlgorythm, value="bubble", font=("Times New Roman", 15)).grid(row=1, column=1, padx=10, pady=10, sticky=W)
+    Radiobutton(top, text="Sortowanie przez wstawianie", variable=sortingAlgorythm, value="insertion", font=("Times New Roman", 15)).grid(row=2, column=1, padx=10, pady=10, sticky=W)
+    Radiobutton(top, text="Sortowanie przez wybieranie", variable=sortingAlgorythm, value="selection", font=("Times New Roman", 15)).grid(row=3, column=1, padx=10, pady=10, sticky=W)
+    Radiobutton(top, text="Bogo sort", variable=sortingAlgorythm, value="bogo", font=("Times New Roman", 15)).grid(row=4, column=1, padx=10, pady=10, sticky=W)
+    Radiobutton(top, text="Cocktail shaker sort", variable=sortingAlgorythm, value="shaker", font=("Times New Roman", 15)).grid(row=5, column=1, padx=10, pady=10, sticky=W)
+    Radiobutton(top, text="Quicksort", variable=sortingAlgorythm, value="quick", font=("Times New Roman", 15)).grid(row=6, column=1, padx=10, pady=10, sticky=W)
+    Radiobutton(top, text="Sortowanie grzebieniowe (comb sort)", variable=sortingAlgorythm, value="comb", font=("Times New Roman", 15)).grid(row=7, column=1, padx=10, pady=10, sticky=W)
+    Radiobutton(top, text="Sortowanie pozycyjne (radix sort)", variable=sortingAlgorythm, value="radix", font=("Times New Roman", 15)).grid(row=8, column=1, padx=10, pady=10, sticky=W)
 
-Label(top, text="Wybierz prędkość sortowania (niższa liczba - szybsze sortowanie):", font=("Times New Roman", 15)).grid(row=9, column=0, columnspan=2, padx=10, pady=20)
+    iloscLiczb = Entry(top, font=("Times New Roman", 15))
+    iloscLiczb.grid(row=2, column=0, padx=10, pady=10, ipadx=10)
 
-delayVar = DoubleVar(value=0.005)
-delayScale = Scale(top, from_=0.005, to=1.0, digits=4, resolution=0.005, orient=HORIZONTAL, variable=delayVar).grid(row=10, column=0, columnspan=2, padx=10, pady=10, ipadx=100)
+    Label(top, text="Wybierz prędkość sortowania (niższa liczba - szybsze sortowanie):", font=("Times New Roman", 15)).grid(row=9, column=0, columnspan=2, padx=10, pady=20)
 
-gotowy = Button(top, text="Pokaż!", font=("Times New Roman", 15), command=lambda: zamknij(odpowiedz.get())).grid(row=11, column=0, columnspan=2, padx=10, pady=10, ipadx=30)
+    delayVar = DoubleVar(value=0.005)
+    delayScale = Scale(top, from_=0.005, to=1.0, digits=4, resolution=0.005, orient=HORIZONTAL, variable=delayVar)
+    delayScale.grid(row=10, column=0, columnspan=2, padx=10, pady=10, ipadx=100)
 
-top.mainloop()
+    Button(top, text="Pokaż!", font=("Times New Roman", 15), command=lambda: zamknij(sortingAlgorythm.get(), iloscLiczb.get(), delayScale.get())).grid(row=11, column=0, columnspan=2, padx=10, pady=10, ipadx=30)
 
-
-def quick_sort(liczby, low, high):
-    def partition(liczby, low, high):
-        pivot = liczby[high]
-        i = low - 1
-        for j in range(low, high):
-            if liczby[j] <= pivot:
-                i = i + 1
-                (liczby[i], liczby[j]) = (liczby[j], liczby[i])
-        (liczby[i + 1], liczby[high]) = (liczby[high], liczby[i + 1])
-        return i + 1
-
-    WIN.fill(GRAY)
-    for index, liczba in enumerate(liczby):
-        x = BLOK_X * index
-        y = HEIGHT - (BLOK_Y * liczba)
-        pygame.draw.rect(WIN, WHITE, (x, y, BLOK_X, BLOK_Y * liczba))
-
-    try:
-        x = BLOK_X * low
-        y = HEIGHT - (BLOK_Y * liczby[low])
-        pygame.draw.rect(WIN, GREEN, (x, y, BLOK_X, BLOK_Y * liczby[low]))
-    except IndexError:
-        pass
-    
-    x = BLOK_X * high
-    y = HEIGHT - (BLOK_Y * liczby[high])
-    pygame.draw.rect(WIN, GREEN, (x, y, BLOK_X, BLOK_Y * liczby[high]))
-
-    pygame.display.update()
-    sleep(DELAY)
-
-    if low < high:
-        pi = partition(liczby, low, high)
-        quick_sort(liczby, low, pi - 1)
-        quick_sort(liczby, pi + 1, high)
+    top.mainloop()
 
 
-def szybszy_bubble_sort():
-    for i in range(ILOSC_LICZB - 1):
+def quick_sort(WIN, clock, liczby):
+    def quick_sort_rec(liczby, low, high):
+        def partition(liczby, low, high):
+            pivot = liczby[high]
+            i = low - 1
+            for j in range(low, high):
+                if liczby[j] <= pivot:
+                    i = i + 1
+                    liczby[i], liczby[j] = liczby[j], liczby[i]
+            liczby[i + 1], liczby[high] = liczby[high], liczby[i + 1]
+            return i + 1
+        
+        try:
+            liczby[low]
+        except IndexError:
+            draw(WIN, liczby, [liczby[high]])
+        else:
+            draw(WIN, liczby, [liczby[low], liczby[high]])
+        clock.tick(SETTINGS["FPS"])
+
+        if low < high:
+            pi = partition(liczby, low, high)
+            quick_sort_rec(liczby, low, pi - 1)
+            quick_sort_rec(liczby, pi + 1, high)
+    quick_sort_rec(liczby, 0, len(liczby) - 1)
+
+
+def szybszy_bubble_sort(WIN, clock, liczby):
+    for i in range(len(liczby) - 1):
         swapped = False
-        for j in range(ILOSC_LICZB - i - 1):
+        for j in range(len(liczby) - i - 1):
             if liczby[j] > liczby[j+1]:
+                draw(WIN, liczby, [liczby[j], liczby[j+1]])
+                clock.tick(SETTINGS["FPS"])
+                
                 swapped = True
                 liczby[j], liczby[j+1] = liczby[j+1], liczby[j]
-
-                draw(liczby)
-                x = BLOK_X * (j + 1)
-                y = HEIGHT - (BLOK_Y * liczby[j+1])
-                pygame.draw.rect(WIN, GREEN, (x, y, BLOK_X, BLOK_Y * liczby[j+1]))
-                x = BLOK_X * (j)
-                y = HEIGHT - (BLOK_Y * liczby[j])
-                pygame.draw.rect(WIN, GREEN, (x, y, BLOK_X, BLOK_Y * liczby[j]))
-                pygame.display.update()
-                sleep(DELAY)
 
         if not swapped:
             break
 
 
-def insertion_sort():
-    for i in range(1, ILOSC_LICZB):
-
+def insertion_sort(WIN, clock, liczby):
+    for i in range(1, len(liczby)):
         pom = liczby[i]
         j = i - 1
         while j >= 0 and liczby[j] > pom:
             liczby[j + 1] = liczby[j]
             j -= 1
-
-            draw(liczby)
-            x = BLOK_X * j
-            y = HEIGHT - (BLOK_Y * liczby[j])
-            pygame.draw.rect(WIN, GREEN, (x, y, BLOK_X, BLOK_Y * liczby[j]))
-            pygame.display.update()
-            sleep(DELAY)
-
-        liczby[j+1] = pom
+            
+            draw(WIN, liczby, [liczby[j]])
+            clock.tick(SETTINGS["FPS"])
+            
+        liczby[j + 1] = pom
 
 
-def selection_sort():
-    for i in range(0, ILOSC_LICZB):
-        minimum = min(liczby[i:ILOSC_LICZB])
-
-        for j in range(i, ILOSC_LICZB):
-            if liczby[j] == minimum:
-                draw(liczby)
-                x = BLOK_X * j
-                y = HEIGHT - (BLOK_Y * liczby[j])
-                pygame.draw.rect(WIN, GREEN, (x, y, BLOK_X, BLOK_Y * liczby[j]))
-                pygame.display.update()
-
-                pom = liczby[j]
-                liczby[j] = liczby[i]
-                liczby[i] = pom
-
-                sleep(DELAY)
+def selection_sort(WIN, clock, liczby):
+    for i in range(0, len(liczby)):
+        minimum = min(liczby[i:len(liczby)])
+        index = liczby.index(minimum)
+        
+        draw(WIN, liczby, [liczby[index]])
+        clock.tick(SETTINGS["FPS"])
+        
+        liczby[i], liczby[index] = liczby[index], liczby[i]
 
 
-def bogo_sort():
-    def posortowane():
-        for i in range(0, len(liczby) - 1):
-            if liczby[i] > liczby[i + 1]:
-                return False
-        return True
+def bogo_sort(WIN, clock, liczby):
+    def is_sorted(lista):
+        return liczby == sorted(lista)
 
-    while not posortowane():
+    while not is_sorted(liczby):
         shuffle(liczby)
-
-        draw(liczby)
-        pygame.display.update()
-        sleep(DELAY)
+        draw(WIN, liczby)
+        clock.tick(SETTINGS["FPS"])
 
 
-def shaker_sort():
-    swap = True
+def shaker_sort(WIN, clock, liczby):
     start = 0
-    end = ILOSC_LICZB - 1
-
-    while (swap == True):
+    end = len(liczby) - 1
+    
+    epilepsja = True
+    swap = True
+    while swap:
         swap = False
 
-        draw(liczby)
-        pygame.display.update()
-        sleep(DELAY)
+        draw(WIN, liczby)
+        clock.tick(SETTINGS["FPS"])
 
         for a in range(start, end):
-            if (liczby[a] > liczby[a+1]):
+            if liczby[a] > liczby[a+1]:
                 liczby[a], liczby[a+1] = liczby[a+1], liczby[a]
                 swap = True
 
-        if(swap == False):
+        if not swap:
             end -= 1
+        
+        if epilepsja:
+            draw(WIN, liczby)
+            clock.tick(SETTINGS["FPS"])
 
         for a in range(end - 1, start - 1, -1):
-            if(liczby[a] > liczby[a+1]):
+            if liczby[a] > liczby[a+1]:
                 liczby[a], liczby[a+1] = liczby[a+1], liczby[a]
                 swap = True
 
         start += 1
 
 
-def comb_sort():
+def comb_sort(WIN, clock, liczby):
     gap = len(liczby)
     shrink = 1.3
-    sorted = False
     
+    sorted = False
     while not sorted:
         gap = int(gap / shrink)
         if gap < 1:
@@ -214,12 +188,11 @@ def comb_sort():
                 sorted = False
             i += 1
             
-            draw(liczby)
-            pygame.display.update()
-            sleep(DELAY // 2)
+            draw(WIN, liczby)
+            clock.tick(SETTINGS["FPS"])
 
 
-def radix_sort():
+def radix_sort(WIN, clock, liczby):
     def countingSort(arr, exp1):
         n = len(arr)
 
@@ -241,9 +214,8 @@ def radix_sort():
         for i in range(0, len(arr)):
             arr[i] = output[i]
 
-            draw(liczby)
-            pygame.display.update()
-            sleep(DELAY)
+            draw(WIN, liczby)
+            clock.tick(SETTINGS["FPS"])
 
     max1 = max(liczby)
     exp = 1
@@ -253,45 +225,71 @@ def radix_sort():
         exp *= 10
 
 
-def draw(liczby):
+def draw(WIN, liczby, highlights = []):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-
-    WIN.fill(GRAY)
+    
+    blokX = SETTINGS["blokX"]
+    blokY = SETTINGS["blokY"]
+    WIN.fill(COLORS["GRAY"])
+    
     for index, liczba in enumerate(liczby):
-        x = BLOK_X * index
-        y = HEIGHT - (BLOK_Y * liczba)
-        pygame.draw.rect(WIN, WHITE, (x, y, BLOK_X, BLOK_Y * liczba))
-        # moveX = round(BLOK_X, 0) // 2
-        # pygame.draw.line(WIN, WHITE, (BLOK_X * index + moveX, HEIGHT), (BLOK_X * index + moveX, HEIGHT - (BLOK_Y * liczba)), int(round(BLOK_X, 0)))
+        if liczba in highlights:
+            color = COLORS["GREEN"]
+        else:
+            color = COLORS["WHITE"]
+        x = blokX * index
+        y = HEIGHT - (blokY * liczba)
+        pygame.draw.rect(WIN, color, (x, y, blokX, blokY * liczba))
+    
+    pygame.display.update()
 
 
 def main():
-    wybor = var.get()
     
-    match wybor:
+    sortingAlgorythm = SETTINGS["algorythm"]
+    iloscLiczb = SETTINGS["iloscLiczb"]
+    SETTINGS["FPS"] = 1 / SETTINGS["delay"]
+    
+    WIN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption("SORTOWANIE")
+    clock = pygame.time.Clock()
+
+    liczby = [i + 1 for i in range(iloscLiczb)]
+    shuffle(liczby)
+    
+    SETTINGS["blokX"] = WIDTH / iloscLiczb
+    SETTINGS["blokY"] = HEIGHT / max(liczby)
+    
+    match sortingAlgorythm:
         case "bubble":
-            szybszy_bubble_sort()
+            szybszy_bubble_sort(WIN, clock, liczby)
         case "insertion":
-            insertion_sort()
+            insertion_sort(WIN, clock, liczby)
         case "selection":
-            selection_sort()
+            selection_sort(WIN, clock, liczby)
         case "bogo":
-            bogo_sort()
+            bogo_sort(WIN, clock, liczby)
         case "shaker":
-            shaker_sort()
+            shaker_sort(WIN, clock, liczby)
         case "quick":
-            quick_sort(liczby, 0, ILOSC_LICZB - 1)
+            quick_sort(WIN, clock, liczby)
         case "comb":
-            comb_sort()
+            comb_sort(WIN, clock, liczby)
         case "radix":
-            radix_sort()
+            radix_sort(WIN, clock, liczby)
         case _:
             pygame.quit()
     
-    draw(liczby)
-    pygame.display.update()
+    for i in range(len(liczby)):
+        if i == 0:
+            draw(WIN, liczby, [liczby[i], liczby[i+1]])
+        elif i == len(liczby) - 1:
+            draw(WIN, liczby, [liczby[i], liczby[i-1]])
+        else:
+            draw(WIN, liczby, [liczby[i-1], liczby[i], liczby[i+1]])
+    draw(WIN, liczby)
     
     while True:
         event = pygame.event.wait()
@@ -299,19 +297,6 @@ def main():
             break
 
 
-if ILOSC_LICZB != 0:
-    WIN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-    pygame.display.set_caption("SORTOWANIE")
-    WIDTH, HEIGHT = WIN.get_size()
-
-    DELAY = delayVar.get()
-
-    liczby = [i + 1 for i in range(ILOSC_LICZB)]
-    shuffle(liczby)
-
-    MAX_WARTOSC = max(liczby)
-    BLOK_X = WIDTH / ILOSC_LICZB
-    BLOK_Y = HEIGHT / MAX_WARTOSC
-
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    settings()
+    main()
